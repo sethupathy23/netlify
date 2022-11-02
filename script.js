92d869bd58f9c00e405dd4cd46b229d41add36ff
sethupathy23 <109471749+sethupathy23@users.noreply.github.com>
2022-11-02 22:15:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function foo(){
+    var res1 = document.getElementById("firstname").value;
+    var res1 = document.getElementById("middlename").value;
+    var res1 = document.getElementById("lastname").value;
+    var res1 = document.getElementById("email").value;
+    var res1 = document.getElementById("password").value;
+  
+}
